61ed0a4d605a8bb52a2878ec5d228b38b7ee8b55
Wilfried Reiter <wilfried@creativity4me.com>
2013-09-26 20:46:36 +0200

    fixed language file tl_contacts/languages/de/tl_content.php added template ce_contact.html5

--- system/modules/contacts/config/autoload.php
+++ system/modules/contacts/config/autoload.php
@@ -18,6 +18,7 @@ ClassLoader::addClasses(array
 	'ModuleContact' 		=>	'system/modules/contacts/modules/ModuleContact.php',
 	'ModuleContactGMaps'	=>	'system/modules/contacts/modules/ModuleContactGMaps.php',
 	'ModuleContactList'		=>	'system/modules/contacts/modules/ModuleContactList.php',
+	'ContentContact' 		=>	'system/modules/contacts/elements/ContentContact.php',
 ));
 
 /**
@@ -26,6 +27,7 @@ ClassLoader::addClasses(array
 TemplateLoader::addFiles(array
 (
 	'mod_contact'		=> 'system/modules/contacts/templates',
+	'ce_contact'		=> 'system/modules/contacts/templates',
 	'contact_basic'		=> 'system/modules/contacts/templates',
 	'contact_networks'	=> 'system/modules/contacts/templates',
 	'contact_gmaps'		=> 'system/modules/contacts/templates',
